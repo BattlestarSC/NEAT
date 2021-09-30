f7d1882dcc48ba31f57a1ff9deeb0df275ed4920
unknown <empholmi@gmail.com>
2021-09-30 09:07:17 -0500

    created some inidividual constructors

--- NEAT/individual.cpp
+++ NEAT/individual.cpp
@@ -20,13 +20,31 @@ individual::individual(int inSize, int outSize, hyperSingleton *controlSingleton
             con->inputNode = j;
             con->outputNode = i;
             con->weight = this->parameterSingleton->getRandom();
+            con->enable = true;
             this->connections.push_back(con);
         }
     }
 }
 
+// This can be used to preserve an individual across generations or to create a child
+// using only mutation
 individual::individual(individual *cloneParent, bool mutate) {
+    this->inputSize = cloneParent->inputSize;
+    this->outputSize = cloneParent->outputSize;
+    this->parameterSingleton = cloneParent->parameterSingleton;
 
+    for (auto* i : cloneParent->connections) {
+        auto* con = new struct connection;
+        con->inputNode = i->inputNode;
+        con->outputNode = i->outputNode;
+        con->weight = i->weight;
+        con->enable = i->enable;
+        this->connections.push_back(con);
+    }
+
+    if (mutate) {
+        this->mutate();
+    }
 }
 
 individual::individual(individual *parentA, individual *parentB) {
@@ -45,7 +63,48 @@ double individual::getFitness() {
     return this->fitness;
 }
 
+/*
+ * So the important assumption is that both parentA->connections and parentB->connections are in the same order. Therefore,
+ * omitted innovation numbers aren't shared and each genome only needs a single linear scan in theory best case time
+ * We will actually do it in nested wasteful for loops for ease
+ */
 void individual::crossover(individual *parentA, individual *parentB) {
+
+   /*
+    * First, lets copy everything from the more fit parent
+    * This will solve making sure all excess and disjoint genes are included
+    * and it will maintain proper execution order
+    */
+
+    auto* fitParent = (parentA->fitness >= parentB->fitness) ? parentA : parentB;
+    auto* weakParent =(fitParent == parentA) ? parentB : parentA;
+
+    // clear our genes
+    this->connections.clear();
+
+    // copy
+    for (auto* i : fitParent->connections) {
+        auto* con = new struct connection;
+        con->inputNode = i->inputNode;
+        con->outputNode = i->outputNode;
+        con->weight = i->weight;
+        con->enable = i->enable;
+        this->connections.push_back(con);
+    }
+
+    // now crossover matching genes from the less fit parent
+    for (auto* g : weakParent->connections) {
+        for (auto* c : this->connections) {
+            if (g->innovationNumber == c->innovationNumber) {
+                // 50% chance to take weak parent's genes
+                if(this->parameterSingleton->getRandom() < 0.0) {
+                    c->weight = g->weight;
+                    c->enable = g->enable;
+                }
+            }
+        }
+    }
+
 
 }
 
